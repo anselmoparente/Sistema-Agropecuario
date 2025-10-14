7993cae68cca287f0a04d9bfeacdf017fb29a8bd
Anselmo Parente Martins <anselmo.martins@unifametro.edu.br>
2025-10-13 21:17:52 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::redirect("/", "/home");
+
+Route::resource('/home', HomeController::class)->only(('index'));
